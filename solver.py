c50df34359e2750e2eedae6f8e2bed482020834b
cgossmann05 <cgossmann05@gmail.com>
2022-03-21 17:17:54 -0600

    Rename variables

--- solver.py
+++ solver.py
@@ -57,31 +57,31 @@ def solve(bo):
     if not find:
         return True
     else:
-        row, column = find
+        row, col = find
     
     for i in range(1, 10):
-        if valid(bo, i, (row, column)):
-            bo[row][column] = i
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
 
             if solve(bo):
                 return True
             
-            bo[row][column] = 0
+            bo[row][col] = 0
     
     return False
 
 
 
-def valid(bo, num, pos):
+def valid(bo, val, pos):
     
     #check row
     for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
+        if bo[pos[0]][i] == val and pos[1] != i:
             return False
     
     #check columns
     for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
+        if bo[i][pos[1]] == val and pos[0] != i:
             return False
     
     #check box
@@ -90,16 +90,16 @@ def valid(bo, num, pos):
 
     for i in range(box_y * 3, box_y * 3 + 3):
         for j in range(box_x * 3, box_x * 3 + 3):
-            if bo[i][j] == num and (i, j) != pos:
+            if bo[i][j] == val and (i, j) != pos:
                 return False
     
     return True
 
 
 def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
-                return (i, j) # row, column
+    for row in range(len(bo)):
+        for col in range(len(bo[0])):
+            if bo[row][col] == 0:
+                return (row, col)
     
     return None
